f7f6a1787840886cfb6fd0064d025850c582e0d6
isu-nice <isu010614@naver.com>
2023-06-06 14:11:10 +0900

    feat: 엔티티 설정

---
+++ src/main/java/todoapp/todo/entity/Todo.java
@@ -0,0 +1,26 @@
+package todoapp.todo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    private int todoOrder;
+
+    @Column(nullable = false)
+    private boolean completed;
+}
